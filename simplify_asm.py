3c2b5178735e33431d917d96a78c8dcf8a0fc815
Quantum <quantum2048@gmail.com>
2022-01-22 20:36:54 -0500

    Add simplify_asm.py to clean up glibc preprocessed assembly

---
+++ simplify_asm.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import os
+import re
+from pathlib import Path
+
+reglobl = re.compile(r'\.globl\s+(\w+)')
+
+
+def process_file(orig_asm):
+    in_chk = False
+    lines = []
+
+    with open(orig_asm, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            elif '.section' in line:
+                line = line.partition(',')[0]
+            elif '.globl' in line:
+                line = line.partition(';')[0]
+                in_chk = False
+                match = reglobl.match(line)
+                if match:
+                    if '_chk_' in match.group(1):
+                        in_chk = True
+                        continue
+
+                    lines.append(line.rstrip())
+                    lines.append(f'{match.group(1)}:')
+                    continue
+            elif '.cfi_endproc' in line:
+                continue
+
+            if not in_chk:
+                lines.append(line.rstrip())
+    return '\n'.join(lines) + '\n'
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Simply preprocessed glibc assembly files.')
+    parser.add_argument('file', nargs='+', help='files to simplify')
+    parser.add_argument('-o', '--output', type=Path, help='directory to output to')
+    args = parser.parse_args()
+
+    for file in args.file:
+        result = process_file(file)
+        if args.output:
+            with open(args.output / os.path.basename(file), 'w') as f:
+                f.write(result)
+        else:
+            print(result)
+
+
+if __name__ == '__main__':
+    main()
