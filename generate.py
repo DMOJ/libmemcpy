81e3908eba1ae98dc245eca473e4e3b83fac0f46
Quantum <quantum2048@gmail.com>
2022-01-22 23:30:05 -0500

    generate: implement mingw-shim.s and libmemcpy.h autogeneration

---
+++ generate.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import re
+from glob import glob
+from pathlib import Path
+
+DIR = Path(__file__).parent
+reglobl = re.compile(r'\.globl\s+(\w+)')
+
+
+def get_functions():
+    result = []
+
+    for file in glob(str(DIR / 'impls' / '*.S')):
+        with open(file) as f:
+            for line in f:
+                match = reglobl.search(line)
+                if match:
+                    name = match.group(1)
+                    if name.startswith('__mem'):
+                        result.append(name.lstrip('_'))
+    return result
+
+
+def generate_mingw_shim(func):
+    return f'''\
+	.global	{func}
+{func}:
+	pushq	%rdi
+	pushq	%rsi
+	movq	%rcx, %rdi
+	movq	%rdx, %rsi
+	movq	%r8,  %rdx
+	call	__{func}
+	popq	%rsi
+	popq	%rdi
+	ret
+'''
+
+
+def update_header(functions):
+    state = 0
+    preamble = []
+    postamble = []
+
+    with open(DIR / 'libmemcpy.h') as f:
+        for line in f:
+            if state == 0:
+                if '// BEGIN GENERATED CODE' in line:
+                    state = 1
+                preamble.append(line)
+            elif state == 1:
+                if '// END GENERATED CODE' in line:
+                    state = 2
+                    postamble.append(line)
+            else:
+                postamble.append(line)
+
+    with open(DIR / 'libmemcpy.h', 'w') as f:
+        for line in preamble:
+            f.write(line)
+
+        print(file=f)
+        print('#ifndef __MINGW32__', file=f)
+
+        for func in functions:
+            print(f'#define {func} __{func}', file=f)
+
+        print('#endif', file=f)
+        print(file=f)
+
+        for func in functions:
+            print(f'memcpy_t {func};', file=f)
+
+        print(file=f)
+        for line in postamble:
+            f.write(line)
+
+
+def main():
+    functions = get_functions()
+
+    with open(DIR / 'mingw-shim.s', 'w') as f:
+        for func in functions:
+            print(generate_mingw_shim(func), file=f)
+
+    update_header(functions)
+
+
+if __name__ == '__main__':
+    main()
---
+++ libmemcpy.h
@@ -0,0 +1,128 @@
+/**
+ * libmemcpy
+ * Copyright © 2022 Guanzhong Chen
+ * Copyright © 2022 Tudor Brindus
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#ifndef _LIBMEMCPY_H_
+#define _LIBMEMCPY_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void memcpy_t(void *dst, const void *src, size_t len);
+
+// BEGIN GENERATED CODE
+
+#ifndef __MINGW32__
+#define memmove_ssse3_back __memmove_ssse3_back
+#define memmove_ssse3 __memmove_ssse3
+#define mempcpy_avx512_no_vzeroupper __mempcpy_avx512_no_vzeroupper
+#define memmove_avx512_no_vzeroupper __memmove_avx512_no_vzeroupper
+#define memcpy_avx512_no_vzeroupper __memcpy_avx512_no_vzeroupper
+#define mempcpy_ssse3_back __mempcpy_ssse3_back
+#define memcpy_ssse3_back __memcpy_ssse3_back
+#define mempcpy_sse2_unaligned __mempcpy_sse2_unaligned
+#define memmove_sse2_unaligned __memmove_sse2_unaligned
+#define mempcpy_erms __mempcpy_erms
+#define memmove_erms __memmove_erms
+#define memcpy_erms __memcpy_erms
+#define mempcpy_sse2_unaligned_erms __mempcpy_sse2_unaligned_erms
+#define memmove_sse2_unaligned_erms __memmove_sse2_unaligned_erms
+#define memcpy_sse2_unaligned_erms __memcpy_sse2_unaligned_erms
+#define memcpy_sse2_unaligned __memcpy_sse2_unaligned
+#define mempcpy_avx512_unaligned __mempcpy_avx512_unaligned
+#define memmove_avx512_unaligned __memmove_avx512_unaligned
+#define mempcpy_avx512_unaligned_erms __mempcpy_avx512_unaligned_erms
+#define memmove_avx512_unaligned_erms __memmove_avx512_unaligned_erms
+#define memcpy_avx512_unaligned_erms __memcpy_avx512_unaligned_erms
+#define memcpy_avx512_unaligned __memcpy_avx512_unaligned
+#define mempcpy_ssse3 __mempcpy_ssse3
+#define memcpy_ssse3 __memcpy_ssse3
+#define mempcpy_evex_unaligned __mempcpy_evex_unaligned
+#define memmove_evex_unaligned __memmove_evex_unaligned
+#define mempcpy_evex_unaligned_erms __mempcpy_evex_unaligned_erms
+#define memmove_evex_unaligned_erms __memmove_evex_unaligned_erms
+#define memcpy_evex_unaligned_erms __memcpy_evex_unaligned_erms
+#define memcpy_evex_unaligned __memcpy_evex_unaligned
+#define mempcpy_avx_unaligned_rtm __mempcpy_avx_unaligned_rtm
+#define memmove_avx_unaligned_rtm __memmove_avx_unaligned_rtm
+#define mempcpy_avx_unaligned_erms_rtm __mempcpy_avx_unaligned_erms_rtm
+#define memmove_avx_unaligned_erms_rtm __memmove_avx_unaligned_erms_rtm
+#define memcpy_avx_unaligned_erms_rtm __memcpy_avx_unaligned_erms_rtm
+#define memcpy_avx_unaligned_rtm __memcpy_avx_unaligned_rtm
+#define mempcpy_avx_unaligned __mempcpy_avx_unaligned
+#define memmove_avx_unaligned __memmove_avx_unaligned
+#define mempcpy_avx_unaligned_erms __mempcpy_avx_unaligned_erms
+#define memmove_avx_unaligned_erms __memmove_avx_unaligned_erms
+#define memcpy_avx_unaligned_erms __memcpy_avx_unaligned_erms
+#define memcpy_avx_unaligned __memcpy_avx_unaligned
+#endif
+
+memcpy_t memmove_ssse3_back;
+memcpy_t memmove_ssse3;
+memcpy_t mempcpy_avx512_no_vzeroupper;
+memcpy_t memmove_avx512_no_vzeroupper;
+memcpy_t memcpy_avx512_no_vzeroupper;
+memcpy_t mempcpy_ssse3_back;
+memcpy_t memcpy_ssse3_back;
+memcpy_t mempcpy_sse2_unaligned;
+memcpy_t memmove_sse2_unaligned;
+memcpy_t mempcpy_erms;
+memcpy_t memmove_erms;
+memcpy_t memcpy_erms;
+memcpy_t mempcpy_sse2_unaligned_erms;
+memcpy_t memmove_sse2_unaligned_erms;
+memcpy_t memcpy_sse2_unaligned_erms;
+memcpy_t memcpy_sse2_unaligned;
+memcpy_t mempcpy_avx512_unaligned;
+memcpy_t memmove_avx512_unaligned;
+memcpy_t mempcpy_avx512_unaligned_erms;
+memcpy_t memmove_avx512_unaligned_erms;
+memcpy_t memcpy_avx512_unaligned_erms;
+memcpy_t memcpy_avx512_unaligned;
+memcpy_t mempcpy_ssse3;
+memcpy_t memcpy_ssse3;
+memcpy_t mempcpy_evex_unaligned;
+memcpy_t memmove_evex_unaligned;
+memcpy_t mempcpy_evex_unaligned_erms;
+memcpy_t memmove_evex_unaligned_erms;
+memcpy_t memcpy_evex_unaligned_erms;
+memcpy_t memcpy_evex_unaligned;
+memcpy_t mempcpy_avx_unaligned_rtm;
+memcpy_t memmove_avx_unaligned_rtm;
+memcpy_t mempcpy_avx_unaligned_erms_rtm;
+memcpy_t memmove_avx_unaligned_erms_rtm;
+memcpy_t memcpy_avx_unaligned_erms_rtm;
+memcpy_t memcpy_avx_unaligned_rtm;
+memcpy_t mempcpy_avx_unaligned;
+memcpy_t memmove_avx_unaligned;
+memcpy_t mempcpy_avx_unaligned_erms;
+memcpy_t memmove_avx_unaligned_erms;
+memcpy_t memcpy_avx_unaligned_erms;
+memcpy_t memcpy_avx_unaligned;
+
+// END GENERATED CODE
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LIBMEMCPY_H_ */
